99e2b54db80ad8f07a719f80802c44b69f2e208f
Pauliina Pietilä <pietila.pauliina@gmail.com>
2026-02-27 22:16:07 +0200

    added health handling to GameManager

--- peliprojekti/Code/GameManager.cs
+++ peliprojekti/Code/GameManager.cs
@@ -2,8 +2,14 @@ using Godot;
 using System;
 using System.Collections;
 
+/// <summary>
+/// GameManager huolehtii pelisessioon liittyvästä datasta.
+/// </summary>
 public partial class GameManager : Node
 {
+    #region Singleton
+
+
     public static GameManager Instance
     {
         get;
@@ -21,5 +27,43 @@ public partial class GameManager : Node
             QueueFree();
             return;
         }
+    }
+    #endregion
+
+    #region Game Data
+    private int _health = 0;
+
+    public int Health
+    {
+        get {return _health;}
+        set
+        {
+            //to do: mieti onko järkevä maksimiarvo
+            _health = Mathf.Clamp(value, 0, 10);
+            //to do: päivitä pisteet käyttöliittymälle
+        }
+    }
+    #endregion
+
+    public bool AddHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            return false;
+        }
+
+        Health += amount;
+        return true;
+    }
+
+    public bool SubstractHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            return false;
+        }
+
+        Health -= amount;
+        return true;
     }
 }
