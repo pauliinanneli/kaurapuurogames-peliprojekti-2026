fa0a3a3f0d17205c40fce6f9fce28d5ffbbdc6ad
Pauliina Pietilä <pietila.pauliina@gmail.com>
2026-03-15 21:54:15 +0200

    worked on connecting stars and trying to get the game to work out for mobile testing

--- peliprojekti/Code/ConnectStar.cs
+++ peliprojekti/Code/ConnectStar.cs
@@ -5,29 +5,102 @@ public partial class ConnectStar : Area2D
 {
     [Export] public int _starIndex;
     [Export] public PointLight2D _light;
+    [Export] public int _starsNeededToFinish = 7;
+    [Export] public int _loopBackIndex = 3;
+
     public bool IsLit = false;
+
     private static int _currentIndex;
+    private static Line2D _line; // line that will connect all the stars to each other
+    private bool _isDragging = false;
 
     public override void _Ready()
     {
         if (_starIndex == 0)
         {
             _currentIndex = 0; //reset counter when level starts
+
+            _line = GetParent().GetNode<Line2D>("Line2D"); // gets line from parent node
+            _line.ClearPoints(); // deletes old points from line2d
+            _line.AddPoint(Position); // add first point to the middle of first star
         }
         MouseEntered += OnTouch;
     }
 
+    public override void _Process(double delta)
+    {
+        if (IsLit && _starIndex == _currentIndex - 1) // only last lit star can start stretching line
+        {
+            // if finger is down, move the line
+            if (Input.IsMouseButtonPressed(MouseButton.Left))
+            {
+                UpdateStretchyLine();
+            }
+        }
+    }
+
+    private void UpdateStretchyLine()
+    {
+        Vector2 mousePosition = _line.GetLocalMousePosition();
+
+        if (_line.GetPointCount() <= _currentIndex)
+        {
+            _line.AddPoint(mousePosition);
+        }
+        else
+        {
+            _line.SetPointPosition(_line.GetPointCount() - 1, mousePosition);
+        }
+    }
+
+    private void RemoveStretchyLine()
+    {
+        // if player stops dragging all of a sudden, delete the stretchy line
+        if (_line.GetPointCount() > _currentIndex)
+        {
+            _line.RemovePoint(_line.GetPointCount() - 1);
+        }
+    }
+
     private void OnTouch()
     {
+        // dont do anything if player isnt touching screen
         if (!Input.IsMouseButtonPressed(MouseButton.Left))
         {
             return;
         }
+
+        // go through stars in order
         if (_starIndex == _currentIndex && !IsLit)
         {
-            LightUp();
-            _currentIndex++;
+            RegisterHit();
         }
+        // closing looping constellation
+        else if (_starIndex == _loopBackIndex && _currentIndex == _starsNeededToFinish)
+        {
+            FinalizeConstellation();
+        }
+    }
+
+    private void RegisterHit()
+    {
+        // snap finger point and snap to stars center
+        if (_line.GetPointCount() > _currentIndex)
+        {
+            _line.SetPointPosition(_line.GetPointCount() - 1, Position);
+        }
+
+        LightUp();
+        _currentIndex++;
+    }
+
+    private void FinalizeConstellation()
+    {
+        if (_line.GetPointCount() > _currentIndex)
+        {
+            _line.SetPointPosition(_line.GetPointCount() - 1, Position);
+        }
+        _currentIndex = 999;
     }
 
     public void LightUp()
