670cbe5b8299f7d5b1fb9ee1b4c73759d3b99eeb
Pauliina Pietilä <pietila.pauliina@gmail.com>
2026-03-16 13:22:24 +0200

    bug fix for connecting stars

--- peliprojekti/Code/ConnectStar.cs
+++ peliprojekti/Code/ConnectStar.cs
@@ -127,8 +127,12 @@ public partial class ConnectStar : Area2D
 
     private void FinalizeConstellation()
     {
+        //snap back to loopback star
+        if (_line != null && _line.GetPointCount() > _currentIndex)
+        {
+            _line.SetPointPosition(_line.GetPointCount() - 1, Position);
+        }
         _currentIndex = 999;
-        // TO DO add logic for when constellation is finished
     }
 
     public void LightUp()
