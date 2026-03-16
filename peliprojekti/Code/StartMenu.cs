eb7a20946683ca300b799c04f52835e94f3ed306
Pauliina Pietilä <pietila.pauliina@gmail.com>
2026-03-16 19:38:53 +0200

    added changing of scene from levels button

--- peliprojekti/Code/StartMenu.cs
+++ peliprojekti/Code/StartMenu.cs
@@ -4,14 +4,15 @@ using System;
 public partial class StartMenu : Control
 {
 	[Export] public string GameScenePath = "res://Scenes/Level1.tscn";
+    [Export] public string LevelsScenePath = "res://Scenes/LevelChooser.tscn";
 
 	public void OnStartButtonPressed()
     {
-        Error result = GetTree().ChangeSceneToFile(GameScenePath);
+        GetTree().ChangeSceneToFile(GameScenePath);
+    }
 
-		if (result != Error.Ok)
-        {
-            GD.PrintErr("Failed to load scene");
-        }
+    public void OnLevelsButtonPressed()
+    {
+        GetTree().ChangeSceneToFile(LevelsScenePath);
     }
 }
