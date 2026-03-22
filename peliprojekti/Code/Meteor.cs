6df4b00640f6c3728b50f84cfc5484f254548513
Pauliina Pietilä <pietila.pauliina@gmail.com>
2026-03-22 20:33:46 +0200

    added collision to meteor

--- peliprojekti/Code/Meteor.cs
+++ peliprojekti/Code/Meteor.cs
@@ -6,6 +6,7 @@ public partial class Meteor : Area2D
 	[Export] public float _floatAmplitude = 150f;
 	[Export] public float _floatSpeed = 2f;
 	[Export] public float _smoothing = 5f;
+	[Export] private float _damageAmount = 0.2f;
 
 	private float _time = 0f;
 	private float _startY;
@@ -22,5 +23,22 @@ public partial class Meteor : Area2D
 		float targetY = _startY + (Mathf.Sin(_time) * _floatAmplitude); //calculate where meteor "wants" to be
 		float finalY = Mathf.Lerp(Position.Y, targetY, _smoothing * (float)delta); // use lerp to smooth movement between current and target y
 		Position = new Vector2(Position.X, finalY);
+    }
+
+    public override void _EnterTree()
+    {
+        BodyEntered += OnBodyEntered;
+    }
+
+	private void OnBodyEntered(Node2D body)
+    {
+        if (body is PlayerCharacter playerCharacter)
+        {
+            //lose health
+			GameManager.Instance.SubstractHealth(_damageAmount);
+			// TO DO add some kind of screen shake / other feedback for hitting comet
+			QueueFree();
+
+        }
     }
 }
