30c954c4cad7ad82c916591daa1127858af86531
Pauliina Pietilä <pietila.pauliina@gmail.com>
2026-03-30 20:29:33 +0300

    adjusted game speed, speed of meteors, health draining all the time and health lost by hitting a meteor to make game easier

--- peliprojekti/Code/GameManager.cs
+++ peliprojekti/Code/GameManager.cs
@@ -73,7 +73,7 @@ public partial class GameManager : Node
     private bool _isDead = false;
 
     private List<string> _personalityChoices = new List<string>(); // list that will store which Muusa role answers correspond with
-    [Export] private float _drainHealth = 0.12f; // loses 10% glow per second, evaluate if thats a smart value or not
+    [Export] private float _drainHealth = 0.08f; // loses 8% glow per second, evaluate if thats a smart value or not
 
     [Export] public Color _defaultColor = Color.FromHtml("#FFD580");
     [Export] public Color _etsijäColor = Color.FromHtml("#00E5FF");
